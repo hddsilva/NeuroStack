fb93c320b7bafc49148486ecd4efb7b04db3352c
Hailey D'Silva <Hailey.Mulder@gmail.com>
2021-10-26 16:54:37 -0400

    Updates to run FreeSurfer

--- neurostack_script.sh
+++ neurostack_script.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-PATH="/usr/lib/afni/bin/:$PATH"
+export PATH="/nitrc/freesurfer/bin/:$PATH"
+export HOME="/nitrc/freesurfer"
 echo "$PATH"
+echo "$HOME"
 aws sts get-caller-identity --query Account --output text > accountID.txt
 accountID=$(cat accountID.txt)
 aSub=$(echo ${FileName}| cut -d. -f1)
@@ -15,16 +17,17 @@ echo "jobQueue: $AWS_BATCH_JQ_NAME"
 echo "computeEnvironment: $AWS_BATCH_CE_NAME"
 
 # --------------- Start user-modified script ---------------
+mkdir ~/fs_input
+export FREESURFER_HOME=~/fs_input
 
 echo "Copying input data onto the instance"
-aws s3 cp s3://neurostack-input-${accountID}/ /usr/lib/afni/bin/ --recursive --exclude "*" --include "${aSub}.nii"
+aws s3 cp s3://neurostack-input-${accountID}/ ~/fs_input/ --recursive --exclude "*" --include "${aSub}.nii"
 
-cd /usr/lib/afni/bin/
-3dcalc -a ${aSub}.nii -prefix ${aSub} -expr 'a'
+echo "Starting recon-all"
+recon-all -subjid ${aSub} -i ~/fs_input/${aSub}.nii -all
 
 echo "Copying output files from the instance to S3 storage"
-aws s3 cp /usr/lib/afni/bin/${aSub}.HEAD s3://neurostack-output-${accountID}/
-aws s3 cp /usr/lib/afni/bin/${aSub}.BRIK s3://neurostack-output-${accountID}/
+aws s3 cp ~/fs_input/ s3://neurostack-output-${accountID}/ --recursive
 
 # --------------- End user-modified script ----------------
 
