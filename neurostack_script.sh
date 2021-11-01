147d360c2f27fac70eb4302510ee730ef7bb0d5b
Hailey D'Silva <Hailey.Mulder@gmail.com>
2021-11-01 16:02:07 -0400

    End of day update

--- neurostack_script.sh
+++ neurostack_script.sh
@@ -8,7 +8,7 @@ aSub=$(echo ${FileName}| cut -d. -f1)
 
 export PATH="/nitrc/usr/local/freesurfer/bin:$PATH"
 echo "$PATH"
-source /nitrc/home/ubuntu/.bashrc
+#source /nitrc/home/ubuntu/.bashrc
 
 echo "Processing subject ${aSub}"
 echo "Args: $@"
@@ -17,29 +17,35 @@ echo "jobQueue: $AWS_BATCH_JQ_NAME"
 echo "computeEnvironment: $AWS_BATCH_CE_NAME"
 
 # --------------- Start user-modified script ---------------
-#echo "Installing Dev Tools"
-#yum -y groupinstall "Development Tools"
-echo "Checking gcc"
-gcc --version
 
-#yum -y install bc
-bc <<< 10*25
-#yum -y install libgomp1
+echo "********************** Original bashrc **********************"
+cat /nitrc/home/ubuntu/.bashrc
 
-export FREESURFER_HOME=/nitrc/usr/local/freesurfer
-echo "Checking FREESURFER_HOME: $FREESURFER_HOME"
-source $FREESURFER_HOME/SetUpFreeSurfer.sh
+echo "********************** New bashrc **********************"
+cat /nitrc/home/bashrc
 
-echo "Copying input data onto the instance"
-aws s3 cp s3://neurostack-input-${accountID}/ $FREESURFER_HOME/ --recursive --exclude "*" --include "${aSub}.nii"
+echo "********************** Moving bashrc **********************"
+rm /nitrc/home/ubuntu/.bashrc
+mv /nitrc/home/bashrc /nitrc/home/ubuntu/.bashrc
 
-echo "Starting recon-all"
-recon-all -subjid ${aSub} -i $FREESURFER_HOME/${aSub}.nii -all
+echo "********************** Final bashrc **********************"
+cat /nitrc/home/ubuntu/.bashrc
 
-echo "Listing output"
-ls -l $FREESURFER_HOME >> output.txt
-
-echo "Copying output files from the instance to S3 storage"
-aws s3 cp $FREESURFER_HOME/output.txt s3://neurostack-output-${accountID}/
-aws s3 cp $FREESURFER_HOME/ s3://neurostack-output-${accountID}/ --recursive --exclude "*" --include "*${aSub}*"
+# echo "Checking FREESURFER_HOME: $FREESURFER_HOME"
+# export FREESURFER_HOME=/nitrc/usr/local/freesurfer
+# echo "Checking FREESURFER_HOME: $FREESURFER_HOME"
+# source $FREESURFER_HOME/SetUpFreeSurfer.sh
+# 
+# echo "Copying input data onto the instance"
+# aws s3 cp s3://neurostack-input-${accountID}/ $FREESURFER_HOME/ --recursive --exclude "*" --include "${aSub}.nii"
+# 
+# echo "Starting recon-all"
+# recon-all -subjid ${aSub} -i $FREESURFER_HOME/${aSub}.nii -all
+# 
+# echo "Listing output"
+# ls -l $FREESURFER_HOME >> output.txt
+# 
+# echo "Copying output files from the instance to S3 storage"
+# aws s3 cp $FREESURFER_HOME/output.txt s3://neurostack-output-${accountID}/
+# aws s3 cp $FREESURFER_HOME/ s3://neurostack-output-${accountID}/ --recursive --exclude "*" --include "*${aSub}*"
 
