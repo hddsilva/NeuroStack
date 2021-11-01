b67f3f1c88447bd3a24f726fddd3ecce1d950462
Hailey D'Silva <Hailey.Mulder@gmail.com>
2021-11-01 10:53:25 -0400

    Updates from Friday

--- neurostack_script.sh
+++ neurostack_script.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-export PATH="/nitrc/freesurfer/bin/:$PATH"
-export HOME="/nitrc/home/ubuntu"
-echo "$PATH"
-echo "$HOME"
 aws sts get-caller-identity --query Account --output text > accountID.txt
 accountID=$(cat accountID.txt)
 aSub=$(echo ${FileName}| cut -d. -f1)
+
+export PATH="/nitrc/usr/local/freesurfer/bin:$PATH"
+echo "$PATH"
 
 echo "Processing subject ${aSub}"
 echo "Args: $@"
@@ -17,13 +16,6 @@ echo "jobQueue: $AWS_BATCH_JQ_NAME"
 echo "computeEnvironment: $AWS_BATCH_CE_NAME"
 
 # --------------- Start user-modified script ---------------
-
-cd ~
-
-echo "Checking for bashrc and profile"
-ls -a
-
-source ~/.bashrc
 
 echo "Checking FREESURFER_HOME"
 cd $FREESURFER_HOME
@@ -40,11 +32,4 @@ ls -l $FREESURFER_HOME >> output.txt
 echo "Copying output files from the instance to S3 storage"
 aws s3 cp $FREESURFER_HOME/output.txt s3://neurostack-output-${accountID}/
 aws s3 cp $FREESURFER_HOME/ s3://neurostack-output-${accountID}/ --recursive --exclude "*" --include "*${aSub}*"
-
-# --------------- End user-modified script ----------------
-
-echo "Hard drive utilization"
-df -h
-
-date
 
