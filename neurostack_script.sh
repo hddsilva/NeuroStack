f059621739a96cca19f288b701dd96748bfa8f3c
Hailey D'Silva <Hailey.Mulder@gmail.com>
2021-11-22 16:34:08 -0500

    Changing Dockerfile to Ubuntu:18.04

--- neurostack_script.sh
+++ neurostack_script.sh
@@ -6,34 +6,40 @@ aws sts get-caller-identity --query Account --output text > accountID.txt
 accountID=$(cat accountID.txt)
 aSub=$(echo ${FileName}| cut -d. -f1)
 
-# TO DO: Need to list out contents of nitrc_bashrc.sh in profile.d to make sure it's there
-# Also make sure environment variables from bashrc are set
-
 #export PATH="/nitrc/usr/local/freesurfer/bin:$PATH"
-echo "Showing custom.sh"
-cat /etc/profile.d/custom.sh
-#source /nitrc/home/ubuntu/.profile
-echo "$PATH"
+#echo "Showing custom.sh"
+#cat /etc/profile.d/custom.sh
+echo "Showing nitrc_bashrc.sh"
+cat /etc/profile.d/nitrc_bashrc.sh
+echo "Showing nitrc_profile.sh"
+cat /etc/profile.d/nitrc_profile.sh
+echo "Path is: $PATH"
+echo "Sourcing bashrc and profile"
+source /etc/profile.d/nitrc_bashrc.sh
+source /etc/profile.d/nitrc_profile.sh
+echo "Path is: $PATH"
 
-echo "Processing subject ${aSub}"
-echo "Args: $@"
-echo "jobId: $AWS_BATCH_JOB_ID"
-echo "jobQueue: $AWS_BATCH_JQ_NAME"
-echo "computeEnvironment: $AWS_BATCH_CE_NAME"
+# echo "Processing subject ${aSub}"
+# echo "Args: $@"
+# echo "jobId: $AWS_BATCH_JOB_ID"
+# echo "jobQueue: $AWS_BATCH_JQ_NAME"
+# echo "computeEnvironment: $AWS_BATCH_CE_NAME"
 
 # --------------- Start user-modified script ---------------
 
 echo "Checking FREESURFER_HOME: $FREESURFER_HOME"
-#export FREESURFER_HOME=/nitrc/usr/local/freesurfer
-#echo "Checking FREESURFER_HOME: $FREESURFER_HOME"
-#source $FREESURFER_HOME/SetUpFreeSurfer.sh
+export FREESURFER_HOME=/nitrc/usr/local/freesurfer
+echo "Checking FREESURFER_HOME: $FREESURFER_HOME"
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
 echo "Copying input data onto the instance"
 aws s3 cp s3://neurostack-input-${accountID}/ $FREESURFER_HOME/ --recursive --exclude "*" --include "${aSub}.nii"
+echo "Copying FreeSurfer license onto the instance"
+aws s3 cp s3://neurostack-script-${accountID}/ $FREESURFER_HOME/ --recursive --exclude "*" --include "license.txt"
 
 echo "Starting recon-all"
 recon-all -subjid ${aSub} -i $FREESURFER_HOME/${aSub}.nii -all
-
+ 
 echo "Listing output"
 ls -l $FREESURFER_HOME >> output.txt
 
