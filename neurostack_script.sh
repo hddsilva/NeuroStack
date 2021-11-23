5b3ae65f9de84f43c72f9f5efd76428c687ec4ed
Hailey D'Silva <Hailey.Mulder@gmail.com>
2021-11-23 11:11:32 -0500

    Fixed filepath for output.txt

--- neurostack_script.sh
+++ neurostack_script.sh
@@ -9,15 +9,15 @@ aSub=$(echo ${FileName}| cut -d. -f1)
 #export PATH="/nitrc/usr/local/freesurfer/bin:$PATH"
 #echo "Showing custom.sh"
 #cat /etc/profile.d/custom.sh
-echo "Showing nitrc_bashrc.sh"
-cat /etc/profile.d/nitrc_bashrc.sh
-echo "Showing nitrc_profile.sh"
-cat /etc/profile.d/nitrc_profile.sh
-echo "Path is: $PATH"
-echo "Sourcing bashrc and profile"
-source /etc/profile.d/nitrc_bashrc.sh
-source /etc/profile.d/nitrc_profile.sh
-echo "Path is: $PATH"
+# echo "Showing nitrc_bashrc.sh"
+# cat /etc/profile.d/nitrc_bashrc.sh
+# echo "Showing nitrc_profile.sh"
+# cat /etc/profile.d/nitrc_profile.sh
+# echo "Path is: $PATH"
+# echo "Sourcing bashrc and profile"
+# source /etc/profile.d/nitrc_bashrc.sh
+# source /etc/profile.d/nitrc_profile.sh
+# echo "Path is: $PATH"
 
 # echo "Processing subject ${aSub}"
 # echo "Args: $@"
@@ -36,12 +36,13 @@ echo "Copying input data onto the instance"
 aws s3 cp s3://neurostack-input-${accountID}/ $FREESURFER_HOME/ --recursive --exclude "*" --include "${aSub}.nii"
 echo "Copying FreeSurfer license onto the instance"
 aws s3 cp s3://neurostack-script-${accountID}/ $FREESURFER_HOME/ --recursive --exclude "*" --include "license.txt"
+#Ran on m4.xlarge
 
 echo "Starting recon-all"
 recon-all -subjid ${aSub} -i $FREESURFER_HOME/${aSub}.nii -all
  
 echo "Listing output"
-ls -l $FREESURFER_HOME >> output.txt
+ls -l $FREESURFER_HOME >> $FREESURFER_HOME/output.txt
 
 echo "Copying output files from the instance to S3 storage"
 aws s3 cp $FREESURFER_HOME/output.txt s3://neurostack-output-${accountID}/
