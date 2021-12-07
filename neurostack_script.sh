c7d200a8246a40bb1ddc6e1f95720e6ace7a4b60
Hailey D'Silva <Hailey.Mulder@gmail.com>
2021-12-07 16:00:01 -0500

    Added scripts to run resting state processing

--- neurostack_script.sh
+++ neurostack_script.sh
@@ -26,25 +26,49 @@ aSub=$(echo ${FileName}| cut -d. -f1)
 # echo "computeEnvironment: $AWS_BATCH_CE_NAME"
 
 # --------------- Start user-modified script ---------------
+export PATH="/nitrc/usr/lib/afni/bin:$PATH"
+export LD_LIBRARY_PATH="/nitrc/lib:/nitrc/usr/lib:/nitrc/usr/lib/afni/lib:/nitrc/usr/lib/x86_64-linux-gnu:/nitrc/lib/x86_64-linux-gnu/"
 
-echo "Checking FREESURFER_HOME: $FREESURFER_HOME"
-export FREESURFER_HOME=/nitrc/usr/local/freesurfer
-echo "Checking FREESURFER_HOME: $FREESURFER_HOME"
-source $FREESURFER_HOME/SetUpFreeSurfer.sh
+cd ~
 
 echo "Copying input data onto the instance"
-aws s3 cp s3://neurostack-input-${accountID}/ $FREESURFER_HOME/ --recursive --exclude "*" --include "${aSub}.nii"
-echo "Copying FreeSurfer license onto the instance"
-aws s3 cp s3://neurostack-script-${accountID}/ $FREESURFER_HOME/ --recursive --exclude "*" --include "license.txt"
-#Ran on m4.xlarge
+aws s3 cp s3://neurostack-input-${accountID}/ ~ --recursive --exclude "*" --include "${aSub}.nii"
 
-echo "Starting recon-all"
-recon-all -subjid ${aSub} -i $FREESURFER_HOME/${aSub}.nii -all
- 
-echo "Listing output"
-ls -l $FREESURFER_HOME >> $FREESURFER_HOME/output.txt
+echo "Starting afni proc py"
+#Modified from example 11b
+afni_proc.py                                                   \
+	 -subj_id ${aSub}                                          \
+	 -blocks despike tshift align tlrc volreg blur mask scale  \
+		 regress                                               \
+	 -copy_anat ${aSub}*anat*.nii.gz                           \
+	 -dsets ${aSub}*rest*.nii.gz                               \
+	 -tcat_remove_first_trs 2                                  \
+	 -align_opts_aea -cost lpc+ZZ                              \
+	 -tlrc_base /home/hm437/abin/TT_N27.nii                    \
+	 -tlrc_NL_warp                                             \
+	 -volreg_align_to MIN_OUTLIER                              \
+	 -volreg_align_e2a                                         \
+	 -volreg_tlrc_warp                                         \
+	 -volreg_warp_dxyz 3                                       \
+	 -blur_size 4                                              \
+	 -mask_segment_anat yes                                    \
+	 -mask_segment_erode yes                                   \
+	 -mask_import Tvent template_ventricle_3mm+tlrc            \
+	 -mask_intersect Svent CSFe Tvent                          \
+	 -mask_epi_anat yes                                        \
+	 -regress_motion_per_run                                   \
+	 -regress_ROI_PC Svent 3                                   \
+	 -regress_ROI_PC_per_run Svent                             \
+	 -regress_make_corr_vols WMe Svent                         \
+	 -regress_anaticor_fast                                    \
+	 -regress_censor_motion 0.2                                \
+	 -regress_censor_outliers 0.05                             \
+	 -regress_apply_mot_types demean deriv                     \
+	 -regress_est_blur_epits                                   \
+	 -regress_est_blur_errts                                   \
+	 -regress_run_clustsim yes                                 \
+	 -bash -execute
 
 echo "Copying output files from the instance to S3 storage"
-aws s3 cp $FREESURFER_HOME/output.txt s3://neurostack-output-${accountID}/
-aws s3 cp $FREESURFER_HOME/ s3://neurostack-output-${accountID}/ --recursive --exclude "*" --include "*${aSub}*"
+aws s3 cp ~/${aSub}*.BRIK s3://neurostack-output-${accountID}/
 
