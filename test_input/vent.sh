6edcbd6097ed7eea5ac16dec05185dad91aaa507
Hailey D'Silva <Hailey.Mulder@gmail.com>
2021-12-21 10:58:53 -0500

    Removing unnecessary files

--- test_input/vent.sh
+++
@@ -1,4 +0,0 @@
-3dcalc -a ~/abin/TT_desai_dd_mpm+tlrc                       \
-                       -expr 'amongst(a,152,170)' -prefix template_ventricle
-3dresample -dxyz 3 3 3 -inset template_ventricle+tlrc \
-                       -prefix template_ventricle_3mm
