401cdb597652923adf043762eb941ec41dd64762
Hailey D'Silva <Hailey.Mulder@gmail.com>
2022-02-03 09:41:37 -0500

    Make aSub command more prominent in script and update bashrc

--- neurostack_script.sh
+++ neurostack_script.sh
@@ -4,18 +4,21 @@ set -e
 
 aws sts get-caller-identity --query Account --output text > accountID.txt
 accountID=$(cat accountID.txt)
-aSub=$(echo ${FileName}| cut -d. -f1)
 
 export PATH="/nitrc/usr/lib/afni/bin:$PATH"
 export LD_LIBRARY_PATH="/nitrc/lib:/nitrc/usr/lib:/nitrc/usr/lib/afni/lib:/nitrc/usr/lib/x86_64-linux-gnu:/nitrc/lib/x86_64-linux-gnu/"
 
-echo "Processing subject ${aSub}"
 echo "Args: $@"
 echo "jobId: $AWS_BATCH_JOB_ID"
 echo "jobQueue: $AWS_BATCH_JQ_NAME"
 echo "computeEnvironment: $AWS_BATCH_CE_NAME"
 
 # --------------- Start user-modified script ---------------
+
+# Defining "aSub" as the name of the input file before the first "."
+aSub=$(echo ${FileName}| cut -d. -f1)
+echo "Processing subject ${aSub}"
+
 cd ~
 
 echo "Copying input data onto the instance"
@@ -27,4 +30,7 @@ echo "Converting from nifti to afni"
 echo "Copying output files from the instance to S3 storage"
 aws s3 cp ~ s3://neurostack-output-${accountID}/ --recursive --exclude "*" --include "${aSub}*.BRIK"
 aws s3 cp ~ s3://neurostack-output-${accountID}/ --recursive --exclude "*" --include "${aSub}*.HEAD"
+
+echo "Processing subject ${aSub}"
+echo "computeEnvironment: $AWS_BATCH_CE_NAME"
 
